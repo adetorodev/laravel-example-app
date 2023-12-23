d6d90656d036e6957f9a6096e9683a445b7ec59a
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-12-23 09:28:46 -0500

    consition rendering

--- routes/web.php
+++ routes/web.php
@@ -21,19 +21,23 @@ Route::get('/home', function () {
     $blogs = [
         [
             "title" => "Title One",
-            "body" => "this is body text"
+            "body" => "this is body text",
+            "status" => 1,
         ],
         [
             "title" => "Title 2",
-            "body" => "this is body text"
+            "body" => "this is body text",
+            "status" => 0,
         ],
         [
             "title" => "Title 3",
-            "body" => "this is body text"
+            "body" => "this is body text",
+            "status" => 1,
         ],
         [
             "title" => "Title 4",
-            "body" => "this is body text"
+            "body" => "this is body text",
+            "status" => 0,
         ]
         ];
     return view('home', compact('blogs'));
