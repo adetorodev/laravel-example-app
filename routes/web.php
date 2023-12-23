7b4bdfd223d7a50ee84327047621d6101ca78ea9
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-12-23 07:20:04 -0500

    Route Grouping

--- routes/web.php
+++ routes/web.php
@@ -17,26 +17,45 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return "<h1>About page</h1>";
-})->name('about');
+// Route::get('/about', function () {
+//     return "<h1>About page</h1>";
+// })->name('about');
 
-Route::get("contact", function() {
-    return "<h1>Contact us page<h1>";
+// Route::get("contact", function() {
+//     return "<h1>Contact us page<h1>";
+// });
+
+// Route::get("contact/{id}", function($id) {
+//     return $id;
+// });
+
+// Route::get("contact/{id}", function($id) {
+//     return $id;
+// })->name('edit-contact');
+
+// Route::get("home", function() {
+//     return "<a href='".route('about')."'>About</a>";
+// });
+
+// Route::get("conta", function($id) {
+//     return "<a href='".route('edit-contact', Sid)."'>Edit Contact</a>";
+// });
+
+// Route Grouping
+Route::prefix('customer')->group(function () {
+    Route::get('/', function () {
+        return "<h1>Customer List</h1>";
+    });
+
+    Route::get('/create', function () {
+        return "<h1>Customer Create</h1>";
+    });
+
+    Route::get('/show', function () {
+        return "<h1>Customer show</h1>";
+    });
 });
 
-Route::get("contact/{id}", function($id) {
-    return $id;
-});
 
-Route::get("contact/{id}", function($id) {
-    return $id;
-})->name('edit-contact');
 
-Route::get("home", function() {
-    return "<a href='".route('about')."'>About</a>";
-});
 
-Route::get("conta", function($id) {
-    return "<a href='".route('edit-contact', Sid)."'>Edit Contact</a>";
-});
