94749b8d727bc55fa29b938d70699f9899c9e1e7
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-12-25 07:58:34 -0500

    login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,29 @@
 @extends('layout.master')
 
 @section('content')
-    <h1>Login Page</h1>
+<div class="row mt-5 justify-content-center">
+    <div class="col-md-6">
+        <h1 class="md-4">Login</h1>
+        <div class="card">
+            <div class="card-body">
+                <form action="">
+                    <div class="mb-2">
+                        <label for="" class="form-label">User Name</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="mb-2">
+                        <label for="" class="form-label">User Email</label>
+                        <input type="email" class="form-control">
+                    </div>
+                    <div class="mb-2">
+                        <label for="" class="form-label">Password</label>
+                        <input type="email" class="form-control">
+                    </div>
+                    <button type="summit" class="btn btn-primary">Summit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+    
 @endsection
