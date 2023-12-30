0ab8f40029f0c30706f13ff7f9c1d8a99cb601b2
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-12-30 18:35:16 -0500

    insert data into db

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,8 +12,27 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return DB::table('posts')->where('id', '>', 20)->where('id', '<', 27)->get();
+        // return DB::table('posts')->where('id', '>', 20)->where('id', '<', 27)->get();
 
-        return view('home', compact('blogs'));
+        // return view('home', compact('blogs'));
+
+        DB::table('posts')->insert([
+            [
+                'title' => 'This is test dATAT',
+            'description' => 'This is dummy data that was insert using the insert type',
+            'status' => 1,
+            'publish_at' => date('Y-m-d'),
+            'user_id' => 1
+            ],
+            [
+                'title' => 'This is test dATAT',
+            'description' => 'This is dummy data that was insert using the insert type',
+            'status' => 1,
+            'publish_at' => date('Y-m-d'),
+            'user_id' => 1
+            ]
+        ]);
+
+        dd('success');
     }
 }
