a07911266d2f1d93ef65e44a96b5a9beb44134b8
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-12-30 18:44:45 -0500

    update data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,27 +12,9 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        // return DB::table('posts')->where('id', '>', 20)->where('id', '<', 27)->get();
-
-        // return view('home', compact('blogs'));
-
-        DB::table('posts')->insert([
-            [
-                'title' => 'This is test dATAT',
-            'description' => 'This is dummy data that was insert using the insert type',
-            'status' => 1,
-            'publish_at' => date('Y-m-d'),
-            'user_id' => 1
-            ],
-            [
-                'title' => 'This is test dATAT',
-            'description' => 'This is dummy data that was insert using the insert type',
-            'status' => 1,
-            'publish_at' => date('Y-m-d'),
-            'user_id' => 1
-            ]
+        DB::table('posts')->where('id', 1)->update([
+            'title' => 'Data Chnaged'
         ]);
-
         dd('success');
     }
 }
