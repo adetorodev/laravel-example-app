b039ccffd06edd351848e09b5e2a246efc0378cf
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-12-31 10:08:23 -0500

    eloquest ORM

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MyPost;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,20 +14,6 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        // DB::table('posts')->where('id', 1)->delete();
-        // DB::table('posts')->delete(2);
-        // return DB::table('posts')->join('Category', 'posts.category_id', '=', 'category.id')
-        // ->select('posts.*') // give onle the post
-        // ->get();
-
-        /** 
-         * count()
-         * max()
-         * min()
-         * avg()
-         * sum()
-         */
-        
-        return DB::tabele('posts')->count();
+        return $posts = MyPost::all(); // get()
     }
 }
